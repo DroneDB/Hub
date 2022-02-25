5e8543594d9643a42b3cdd623f4f1c3fe537b7b8
Luca Di Leo <ldileo@digipa.it>
2022-02-25 10:06:16 +0100

    Autoformat

--- webapp/js/main.js
+++ webapp/js/main.js
@@ -17,17 +17,17 @@ import Upload from './components/Upload.vue';
 import reg from './libs/sharedRegistry';
 import { setTitle, queryParams, inIframe } from './libs/utils';
 
-window.addEventListener('load', function(){
+window.addEventListener('load', function () {
     const isProduction = window.location.href.indexOf("localhost") == -1 &&
-                        window.location.href.indexOf("192.168.") == -1 &&
-                        window.location.href.indexOf("127.0.0.1") == -1;
+        window.location.href.indexOf("192.168.") == -1 &&
+        window.location.href.indexOf("127.0.0.1") == -1;
 
     const options = {
         isEnabled: true,
-        logLevel : isProduction ? 'error' : 'debug',
-        stringifyArguments : false,
-        showLogLevel : true,
-        showMethodName : true,
+        logLevel: isProduction ? 'error' : 'debug',
+        stringifyArguments: false,
+        showLogLevel: true,
+        showMethodName: true,
         separator: '|',
         showConsoleColors: true
     };
@@ -47,14 +47,14 @@ window.addEventListener('load', function(){
 
     Vue.use(VueLogger, options);
     Vue.use(VueRouter);
-    
+
     const routes = [
-        { path: '/r/:org/:ds', name: "ViewDataset", components: {content: ViewDataset, header: hdr}, meta: { title: "View Dataset"}},
-        { path: '/login', name: "Login", components: {content: Login, header: hdr}, meta: { title: "Login" }},
-        { path: '/r/:org', name: "UserHome", components: {content: UserHome, header: hdr}, meta: { title: "Home"}},
-        { path: '/upload', name: "Upload", components: {content: Upload, header: hdr}, meta: { title: "Upload" }},
+        { path: '/r/:org/:ds', name: "ViewDataset", components: { content: ViewDataset, header: hdr }, meta: { title: "View Dataset" } },
+        { path: '/r/:org', name: "UserHome", components: { content: UserHome, header: hdr }, meta: { title: "Home" } },
+        { path: '/login', name: "Login", components: { content: Login, header: hdr }, meta: { title: "Login" } },
+        { path: '/upload', name: "Upload", components: { content: Upload, header: hdr }, meta: { title: "Upload" } },
         // TODO: add an actual home page
-        { path: '/', name: "LoginHome", components: {content: Login, header: hdr}, meta: { title: "Login" }},
+        { path: '/', name: "LoginHome", components: { content: Login, header: hdr }, meta: { title: "Login" } },
         { path: '*', redirect: '/' }
     ];
     const router = new VueRouter({ mode: "history", routes });
@@ -67,10 +67,10 @@ window.addEventListener('load', function(){
     });
 
     // Refresh auth tokens
-    if (reg.isLoggedIn()){
+    if (reg.isLoggedIn()) {
         reg.refreshToken();
         reg.setAutoRefreshToken();
-    }else{
+    } else {
         reg.clearCredentials();
     }
 
@@ -80,9 +80,9 @@ window.addEventListener('load', function(){
 
     Vue.config.errorHandler = function (err, vm, info) {
         // Catch unauthorized error globally
-        if (err.message === "Unauthorized"){
-            router.push({name: "Login"}).catch(()=>{});
-        }else{
+        if (err.message === "Unauthorized") {
+            router.push({ name: "Login" }).catch(() => { });
+        } else {
             throw err;
         }
     }
@@ -91,7 +91,7 @@ window.addEventListener('load', function(){
     document.getElementById("main-loading").style.display = 'none';
 
     // Live reload
-    if (!isProduction){
+    if (!isProduction) {
         const livereload = document.createElement("script");
         livereload.src = `${window.location.protocol}//${window.location.hostname}:35729/livereload.js`;
         document.body.appendChild(livereload);
