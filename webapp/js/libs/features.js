de04ddf80c3abd63d094828f8b76d94ffa1feb16
HeDo <ldileo@digipa.it>
2026-02-06 22:25:58 +0100

    Added feature flags consts

---
+++ webapp/js/libs/features.js
@@ -0,0 +1,9 @@
+/**
+ * Feature flag constants.
+ * These match the properties returned by the /sys/features endpoint.
+ */
+export const Features = {
+    ORGANIZATION_MEMBER_MANAGEMENT: 'organizationMemberManagement',
+    USER_MANAGEMENT: 'userManagement',
+    STORAGE_LIMITER: 'storageLimiter'
+};
