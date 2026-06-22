7d17fa153f2fc074d411a3415ed0c1ec5c2d887b
HeDo <ldileo@digipa.it>
2026-06-22 18:22:06 +0200

    Bumped version to v2.5.2

--- test-setup.js
+++ test-setup.js
@@ -8,7 +8,7 @@ import { vi } from 'vitest';
 globalThis.__APP_PRODUCTION__ = true;
 
 // Mock __HUB_VERSION__ global
-globalThis.__HUB_VERSION__ = '2.5.1';
+globalThis.__HUB_VERSION__ = '2.5.2';
 
 // Mock window/localStorage for Node environment
 if (typeof window === 'undefined') {
