7fc0750240946a33b8be87a3d24c5a5923962ef3
HeDo <ldileo@digipa.it>
2026-08-07 01:55:58 +0200

    Added hangfire and scalar links to the admin menu

--- webapp/js/layout/Header.vue
+++ webapp/js/layout/Header.vue
@@ -304,6 +304,8 @@ export default {
             if (this.isAdmin) {
                 items.push({ label: 'Tasks', icon: 'fa-solid fa-list-check fixed-icon', command: () => this.adminTasks() });
                 items.push({ label: 'Configuration', icon: 'fa-solid fa-gear fixed-icon', command: () => this.adminConfig() });
+                items.push({ label: 'Hangfire', icon: 'fa-solid fa-fire fixed-icon', url: '/hangfire', target: '_blank' });
+                items.push({ label: 'API Docs', icon: 'fa-solid fa-code fixed-icon', url: '/scalar/v1', target: '_blank' });
             }
 
             items.push({ separator: true });
