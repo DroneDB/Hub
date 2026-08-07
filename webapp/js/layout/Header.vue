5f368b5b8054b0c2778fe92f0b38c89013cc4883
HeDo <ldileo@digipa.it>
2026-08-07 03:00:04 +0200

    Improved separation

--- webapp/js/layout/Header.vue
+++ webapp/js/layout/Header.vue
@@ -303,6 +303,7 @@ export default {
             }
             if (this.isAdmin) {
                 items.push({ label: 'Tasks', icon: 'fa-solid fa-list-check fixed-icon', command: () => this.adminTasks() });
+                items.push({ separator: true });
                 items.push({ label: 'Configuration', icon: 'fa-solid fa-gear fixed-icon', command: () => this.adminConfig() });
                 items.push({ label: 'Hangfire', icon: 'fa-solid fa-fire fixed-icon', url: '/hangfire', target: '_blank' });
                 items.push({ label: 'API Docs', icon: 'fa-solid fa-code fixed-icon', url: '/scalar/v1', target: '_blank' });
